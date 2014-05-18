33e7ecc166008014ee224bc27afc62753662a06b
Sam Magura <srmagura@gmail.com>
2014-05-18 17:28:21 -0400

    unit-info-window: "common" and "stats"

--- chapters/chapter1.js
+++ chapters/chapter1.js
@@ -1,6 +1,8 @@
 function initChapter(){
     var playerUnits = [
         new Unit({name: 'Ace', baseHp: 20, move: 4, str: 7, def: 2,
+            skill: 5, speed: 6, res: 2, luck: 8, aid: 7, con: 5,
+            mag: 0,
             lord: true, inventory: [new Item('iron-sword')]}),
         new Unit({name: 'Shiina', baseHp: 16, move: 4, str: 9, def: 1,
             inventory: [new Item('iron-sword')]}),
--- unitInfoWindow.js
+++ unitInfoWindow.js
@@ -1,3 +1,39 @@
 function initUnitInfoWindow(unit){
-    console.log(unit.name)
+    $('.dark-overlay').show()
+
+    var w = $('.unit-info-window')
+    var dw = $(document).width()
+    w.css({visibility: 'visible', left: (dw-w.width())/2, top: 40})
+
+    var nameField = unit.name
+    if(unit.lord){
+        nameField += ' <div class="lord">(Lord)</div>'
+    }
+    w.find('.common .name').html(nameField)
+    w.find('.common .uclass').text('Mercenary')
+    w.find('.common .level').text('1')
+    w.find('.common .exp').text('0')
+
+    var stats = w.find('.tab-content-stats')
+    var statTypes = ['str', 'skill', 'mag', 'speed', 'def', 'luck',
+        'res', 'move', 'aid', 'con']
+
+    for(var k = 0; k < statTypes.length; k++){
+        var st = statTypes[k]
+        stats.find('.' + st).text(unit[st])
+    }
+
+    controlState = csUnitInfoWindow
+}
+
+function closeUnitInfoWindow(){
+    $('.dark-overlay').hide()
+    $('.unit-info-window').css('visibility', 'hidden')
+    controlState = csMap
+}
+
+var csUnitInfoWindow = Object.create(controlState)
+
+csUnitInfoWindow.d = function(){
+    closeUnitInfoWindow()    
 }
