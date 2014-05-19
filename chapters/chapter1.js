91cd73a2d583b83f48334825591c5e415274e4b0
Sam Magura <srmagura@gmail.com>
2014-05-19 01:22:21 -0400

    demo-v0.000001a: buff Ace so he can actually win

--- chapters/chapter1.js
+++ chapters/chapter1.js
@@ -1,7 +1,7 @@
 function initChapter(){
     var playerUnits = [
-        new Unit({name: 'Ace', baseHp: 20, baseMp: 5, 
-            move: 4, str: 7, def: 2,
+        new Unit({name: 'Ace', baseHp: 25, baseMp: 5, 
+            move: 4, str: 7, def: 5,
             skill: 5, speed: 6, res: 2, luck: 8, aid: 7, con: 5,
             mag: 0, picture: true, 
             lord: true, inventory: [new Item('iron-sword')]})
